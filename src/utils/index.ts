3af56a5b59fecda2bd3c1f5b2e9cf38e35acdb20
gpsign <gpsignorini@gmail.com>
2025-03-19 15:45:24 -0300

    feat: Exporting utils object

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
 export * from "./nvl";
 export * from "./fabricate";
 export * from "./types";
+
+import * as fabricate from "./fabricate";
+import * as nvl from "./nvl";
+
+export const utils = {
+  ...fabricate,
+  ...nvl,
+};
