1c5a18bb02ab22ceab85af36a2cf575fa10f8c53
gpsign <gpsignorini@gmail.com>
2025-03-19 10:34:55 -0300

    fix: indexes and types

--- src/hooks/useUpdate.ts
+++ src/hooks/useUpdate.ts
@@ -1,7 +1,39 @@
 import { useId, useMemo, useState } from "react";
 import { UpdateHook } from "./types";
 
-export default function useUpdate(): UpdateHook {
+/**
+ * Hook que retorna uma função para disparar a re-renderização do componente.
+ *
+ * Este hook é ideal para casos em que você deseja atualizar a interface do usuário sem depender de
+ * alterações diretas em um estado que afete a renderização.
+ *
+ * ## Exemplo
+ *
+ *
+ * ```tsx
+ *
+ * function DisplayOnClick() {
+ *   const update = useUpdate();
+ *
+ *   // Cria uma referência para o input. O valor digitado não causará re-renderização ao ser alterado.
+ *   const inputRef = useRef<HTMLInputElement>(null);
+ *
+ *   return (
+ *     <div>
+ *       <input type="text" ref={inputRef} placeholder="Digite algo..." />
+ *       <button onClick={update}>Mostrar valor</button>
+ *       <p>Valor do input: {inputRef.current ? inputRef.current.value : ''}</p>
+ *       <p>Número de atualizações: {update.counter}</p>
+ *       <small>ID do hook: {update.id}</small>
+ *     </div>
+ *   );
+ * }
+ *
+ * ```
+ * @returns Uma função de atualização que, além de disparar a re-renderização, possui as propriedades
+ * `id` e `counter` para facilitar o rastreamento das atualizações.
+ */
+export function useUpdate(): UpdateHook {
   const [counter, setCounter] = useState(0);
   const id = useId();
 
