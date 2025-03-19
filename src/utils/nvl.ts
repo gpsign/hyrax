e076da9775ebc54689fd88e7207ebca02d540bb3
gpsign <gpsignorini@gmail.com>
2025-03-19 15:45:03 -0300

    fix: Fixed nvl generic type

--- src/utils/nvl.ts
+++ src/utils/nvl.ts
@@ -1,15 +1,17 @@
 /**
  * Retorna o primeiro valor que não seja nulo ou indefinido dentre os argumentos fornecidos.
  *
- * @param {...(T | null | undefined)[]} values - Uma lista de valores de qualquer tipo, que podem ser nulos ou indefinidos.
- * @returns {T | null} - O primeiro valor definido encontrado. Se todos os valores forem nulos ou indefinidos, retorna `null`.
+ * @param {...(T[K] | null | undefined)[]} values - Uma lista de valores (em formato de tupla) que podem ser nulos ou indefinidos.
+ * @returns {T[number] | null} - O primeiro valor definido encontrado. Se todos os valores forem nulos ou indefinidos, retorna `null`.
  *
  * ### Exemplo
  * ```ts
  * nvl(null, undefined, 0, "exemplo"); // Saída: 0
  * ```
  */
-export function nvl<T>(...values: Array<T | undefined | null>): T | null {
+export function nvl<T extends unknown[]>(
+  ...values: { [K in keyof T]: T[K] | null | undefined }
+): T[number] | null {
   const result = values.find((value) => value != null);
   return result === undefined ? null : result;
 }
